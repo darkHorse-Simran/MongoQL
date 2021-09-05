dd31a75cb725b9b4706749bb75e7ac8a17074729
Simran Srivastava <simsri2805@gmail.com>
2021-09-05 15:38:22 +0530

    Initialised code structure

---
+++ src/main/java/com/sqltomongo/MongoQL/MongoQlApplication.java
@@ -0,0 +1,13 @@
+package com.sqltomongo.MongoQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoQlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sqltomongo/MongoQL/MongoQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqltomongo.MongoQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
